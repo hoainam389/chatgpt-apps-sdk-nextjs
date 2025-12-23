7c00f8bc5be19514979d62fe6cb50089cad18861
Nam Nguyen <nam.nguyen@electrolux.com>
2025-12-23 17:07:38 +0700

    revert to original

--- middleware.ts
+++ middleware.ts
@@ -12,14 +12,13 @@ export function middleware(request: NextRequest) {
     response.headers.set("Access-Control-Allow-Headers", "*");
     return response;
   }
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,OPTIONS"
-  );
-  response.headers.set("Access-Control-Allow-Headers", "*");
-  return response;
+  return NextResponse.next({
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+    },
+  });
 }
 
 export const config = {
